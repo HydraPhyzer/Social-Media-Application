acc35a3b0ca28ddfa41b8e31f214fbce7306724b
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2023-03-06 10:48:59 +0500

    Designed 404 Page

--- Client/.next/static/chunks/webpack.js
+++ Client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e74f4c03f5687fab"; }
+/******/ 		__webpack_require__.h = function() { return "bd0c350ad3ce920c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Client/pages/404.tsx
+++ Client/pages/404.tsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useMemo } from "react";
+import Image from "next/image";
+import { Box, createTheme, ThemeOptions } from "@mui/system";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { SetMode } from "../Redux/AuthReducer";
+import { ThemeSettings } from "../Components/Themes/Themes";
+import { CustomTheme } from "../Components/Themes/CustomTheme";
+import DarkModeIcon from "@mui/icons-material/DarkMode"
+import { useRouter } from "next/router";
 
 const NotFound = () => {
-  return (
-    <div className='text-center'>Sorry This Page Not Found</div>
-  )
-}
+  const Mode = useSelector((State: any) => State.Mode);
+  const Dispatch = useDispatch();
+  let Router=useRouter();
 
-export default NotFound
+  let SetMod = () => {
+    Dispatch(SetMode());
+  };
+
+  let Theme = useMemo(() => {
+    return createTheme(
+      ThemeSettings(Mode) as unknown as ThemeOptions
+    ) as CustomTheme;
+  }, [Mode]);
+  return (
+    <div style={{ backgroundColor: Theme.Palette.Background.Default }}
+    className="max-h-[100vh] overflow-hidden">
+      <Box
+        className="flex items-center text-center p-3"
+        sx={{
+          backgroundColor: Theme.Palette.Background.Alt,
+          color: Theme.Palette.Primary.Main,
+        }}
+      >
+        <div className="mx-auto">
+          <span>Connectify</span>
+          <Image
+            src="/Favicon/Favicon.png"
+            height={20}
+            width={20}
+            alt="Logo"
+            className="inline mx-1"
+          />
+        </div>
+        <DarkModeIcon
+          onClick={() => {
+            SetMod();
+          }}
+        />
+      </Box>
+
+      <div className="flex flex-col justify-evenly items-center h-[100vh]">
+        <img src="Giphy/Error.gif" alt="" height="30%" width="30%" className="rounded-md" />
+        <button onClick={()=>{Router.push("/")}} className="text-center self-center p-2 rounded-sm text-white" style={{backgroundColor:Theme.Palette.Primary.Main}}>Home Page</button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
